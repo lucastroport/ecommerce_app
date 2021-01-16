06191996ea0f387e21e3376df65e633575926767
lucastroport <federicocastro999@gmail.com>
2021-01-16 14:01:50 -0300

    added order routes in backend and save order logic

---
+++ backend/routes/orderRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import {
+    addOrderItems
+} from '../controllers/OrderController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
+const router = express.Router()
+
+router.post('/').post(protect, addOrderItems)
+
+export default router
