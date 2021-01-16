20b25c6b0887463bffd24117374455a4f52ef3f2
lucastroport <federicocastro999@gmail.com>
2021-01-16 11:01:46 -0300

    added routes to update user profile and method in controller

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,19 @@
 import express from 'express'
-import { authUser, getUserProfile, regsiterUser } from '../controllers/userController.js'
+import {
+    authUser,
+    getUserProfile,
+    regsiterUser,
+    updateUserProfile
+} from '../controllers/userController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
-router.route('/profile').get(protect, getUserProfile)//because its protected we add the protect middleware
+router.route('/profile')
+    .get(protect, getUserProfile)//because its protected we add the protect middleware
+    .put(protect, updateUserProfile)
+
 router.route('/').post(regsiterUser)
 
 export default router
