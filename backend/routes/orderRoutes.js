a30d3c253fe8755e9778abc4e76d7633cea65710
lucastroport <federicocastro999@gmail.com>
2021-01-17 11:46:23 -0300

    added routes for paypal order payment in controller

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import {
     addOrderItems,
-    getOrderById
+    getOrderById,
+    updateOrderToPaid
 } from '../controllers/OrderController.js'
 import { protect } from '../middleware/authMiddleware.js'
 
@@ -9,6 +10,7 @@ const router = express.Router()
 
 router.route('/').post(protect, addOrderItems)
 router.route('/:id').get(protect, getOrderById)
+router.route('/:id/pay').put(protect, updateOrderToPaid)
 
 
 export default router
