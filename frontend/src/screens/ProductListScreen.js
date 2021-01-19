337e09094782548b48521299aaf59ec4aa2f7c82
lucastroport <federicocastro999@gmail.com>
2021-01-19 17:57:38 -0300

    finished implementing product delete

--- frontend/src/screens/ProductListScreen.js
+++ frontend/src/screens/ProductListScreen.js
@@ -4,7 +4,7 @@ import { Table, Button, Row, Col } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
-import { listProducts } from '../actions/productActions'
+import { listProducts, deleteProduct } from '../actions/productActions'
 import Swal from 'sweetalert2'
 
 
@@ -15,6 +15,9 @@ const ProductListScreen = ({ history, match }) => {
     const productList = useSelector(state => state.productList)
     const { loading, error, products } = productList
 
+    const productDelete = useSelector(state => state.productDelete)
+    const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete
+
     const userLogin = useSelector(state => state.userLogin)
     const { userInfo } = userLogin
 
@@ -24,7 +27,7 @@ const ProductListScreen = ({ history, match }) => {
         } else {
             history.push('/login')
         }
-    }, [dispatch, history, userInfo])
+    }, [dispatch, history, userInfo, successDelete])
 
     const deleteHandler = (id) => {
 
@@ -36,7 +39,7 @@ const ProductListScreen = ({ history, match }) => {
             icon: 'warning'
         }).then((result) => {
             if (result.isConfirmed) {
-                //DELETE PRODUCT
+                dispatch(deleteProduct(id))
                 Swal.fire('Deleted!', '', 'success')
             } else if (result.isDismissed) {
                 Swal.fire('Changes are not saved', '', 'info')
@@ -62,6 +65,8 @@ const ProductListScreen = ({ history, match }) => {
                 </Button>
                 </Col>
             </Row>
+            {loadingDelete && <loader />}
+            {errorDelete && <Message variant='danger'>{errorDelete}</Message>}
             {loading ? <Loader />
                 : error
                     ? <Message variant='danger'>{error}</Message>
