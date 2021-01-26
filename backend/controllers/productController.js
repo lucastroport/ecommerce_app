6cacf357cc82c258783ac193f73a5fa753bb8547
lucastroport <federicocastro999@gmail.com>
2021-01-26 20:16:36 -0300

    added pagination both in admin productlist and user product list

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -6,6 +6,9 @@ import asyncHandler from 'express-async-handler'
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
 
+    const pageSize = 10
+    const page = Number(req.query.pageNumber) || 1
+
     const keyword = req.query.keyword
         ? {
             name: {
@@ -14,9 +17,13 @@ const getProducts = asyncHandler(async (req, res) => {
             }
         } : {}
 
+    const count = await Product.countDocuments({ ...keyword })
     const products = await Product.find({ ...keyword })
+        .limit(pageSize)
+        .skip(pageSize * (page - 1))
 
-    res.json(products)
+
+    res.json({ products, page, pages: Math.ceil(count / pageSize) })
 })
 
 
